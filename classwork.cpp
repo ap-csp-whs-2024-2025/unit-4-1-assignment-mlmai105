38f05eb864cd95fa8ddfe1a903a8e3a2c4d5a951
mlmai105 <mollyle43@gmail.com>
2024-12-11 20:57:51 +0000

    leabt abt output

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,11 @@
 
 int main()
 {
-  // write code here
-  
+  int age = 10;
+  double price = 19.99;
+  bool isRaining = false; 
+
+  std::cout << age << "\n" << price << std::endl << isRaining;
+
   return 0;
 }
