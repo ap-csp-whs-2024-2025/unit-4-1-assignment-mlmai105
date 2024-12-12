2a4d34ef7e25c9f38a4b70199c12cbd35f8501bf
mlmai105 <mollyle43@gmail.com>
2024-12-12 21:49:32 +0000

    class

--- classwork.cpp
+++ classwork.cpp
@@ -3,14 +3,32 @@
 
 int main()
 {
-  int age = 10;
-  double price = 19.99;
-  bool isRaining = false; 
+  
+  // int num = 5;
+  // num = 10;
 
-  std::cout << age << "\n" << price << std::endl << isRaining;
+  // std::string stu_name = "John";
+  // std::string number = "5";
 
-  int grade_level = 12;
-  std::cout << "I am a " << grade_level << "th_grader." << std::endl; 
+  // std::cout << "My name is" << stu_name << std::endl;
+  // double price = 19.99;
+  // bool isRaining = false;
 
-  return 0;
+  // std::cout << age << "\n" << price << std::endl << isRaining;
+
+  // int grade_level = 12;
+  // std::cout << "I am a " << grade_level << "th_grader." << std::endl;
+
+//exercise 1
+  int x; 
+  int y;
+  x = 25;
+  y = 3;
+  int add_xy = x + y;
+  std::cout << "x + y = " << add_xy << std::endl;
+  std::cout << "x - y = " << x - y << std::endl;
+  std::cout << "x * y = " << x * y << std::endl;
+  std::cout << "x / y = " << x / y << std:: endl;
 }
+
+  
