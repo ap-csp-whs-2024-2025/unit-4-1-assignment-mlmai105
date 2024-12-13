635f4b850bd102a4cace73595d7e0610b6b74589
mlmai105 <mollyle43@gmail.com>
2024-12-13 21:49:44 +0000

    classwork3

--- classwork.cpp
+++ classwork.cpp
@@ -20,15 +20,50 @@ int main()
   // std::cout << "I am a " << grade_level << "th_grader." << std::endl;
 
 //exercise 1
-  int x; 
-  int y;
-  x = 25;
-  y = 3;
-  int add_xy = x + y;
-  std::cout << "x + y = " << add_xy << std::endl;
-  std::cout << "x - y = " << x - y << std::endl;
-  std::cout << "x * y = " << x * y << std::endl;
-  std::cout << "x / y = " << x / y << std:: endl;
+  // int x; 
+  // int y;
+  // int z;
+  // z = 10;
+  // x = 25;
+  // y = 3;
+  // int add_xy = x + y;
+  // std::cout << "x + y = " << add_xy << std::endl;
+  // std::cout << "x - y = " << x - y << std::endl;
+  // std::cout << "x * y = " << x * y << std::endl;
+  // std::cout << "x / y = " << x / y << std::endl;
+
+  // std::cout << "x > z = " << (x > z) << std::endl;
+  // std::cout << "y < x = " << (y < x) << std::endl;
+
+  // std::cout << std::endl;
+
+ 
+
+  //exercise 2 
+  // bool isSunny;
+  // bool hasUmbrella;
+  // isSunny = true;
+  // hasUmbrella = false;
+
+  // std::cout << "isSunny && hasUmbrella: " << (isSunny && hasUmbrella) << std::endl;
+  // std::cout << "isSunny || hasUmbrella: " << (isSunny || hasUmbrella) << std::endl;
+  // std::cout << "!isSunny: " << (!isSunny) << std::endl;
+  // std::cout << "!isSunny && !hasUmbrella: " << (!isSunny && !hasUmbrella) << std::endl;
+
+  //exercise 3 
+  int n = 7;
+  double m = 4.0;
+  bool hasLicense = false;
+
+  std::cout << "n > m = " << (n > m) << std::endl; 
+  std::cout << "m = " << (m) << std::endl;
+  std::cout << "hasLicense && m <= n: " << (hasLicense && m <= n) << std::endl;
+  std::cout << "n > 5 && hasLicense: " << (n > 5 && hasLicense) << std::endl;
+
+
+
+
+  return 0;
 }
 
   
